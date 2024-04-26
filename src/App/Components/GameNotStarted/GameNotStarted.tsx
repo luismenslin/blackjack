5698f307f412de4031cfa6cf5cfd24173ae94237
Luis <luis.fmenslinx@gmail.com>
2024-04-25 21:31:14 -0300

    feat: bet validation

--- src/App/Components/GameNotStarted/GameNotStarted.tsx
+++ src/App/Components/GameNotStarted/GameNotStarted.tsx
@@ -3,14 +3,16 @@ import React, { Dispatch, SetStateAction } from "react";
 import Button from "../Button";
 import MoneyInput from "../MoneyInput";
 import { formatCurrency } from "../../../utils/formatUtils";
+import {isNumber} from "util";
 
 interface GameNotStartedProps {
+    betValue: number
     setBetValue: Dispatch<SetStateAction<number>>
     setIsGameStarted: Dispatch<SetStateAction<boolean>>
     valueOwn: number
 }
 
-const GameNotStarted = ({valueOwn, setBetValue, setIsGameStarted}: GameNotStartedProps) => {
+const GameNotStarted = ({valueOwn, setBetValue, setIsGameStarted, betValue}: GameNotStartedProps) => {
     const validateValue = (betValue: number | undefined): void => {
         if (betValue) setBetValue(betValue)
     }
@@ -22,7 +24,13 @@ const GameNotStarted = ({valueOwn, setBetValue, setIsGameStarted}: GameNotStarte
             <p className={styles.text}>Faça uma aposta<br/> Você tem {formatCurrency(valueOwn, true)}</p>
             <br/>
             <MoneyInput onValueChange={validateValue} />
-            <Button label={"Apostar e iniciar jogo!"} onClick={() => setIsGameStarted(true)}/>
+            <Button label={"Apostar e iniciar jogo!"} onClick={() => {
+                if (betValue <= 0 || betValue > valueOwn) {
+                    alert("Da uma olhada nessa aposta ai meu parceiro ༼ つ ◕_◕ ༽つ");
+                } else {
+                    setIsGameStarted(true)
+                }
+            }}/>
         </div>
     )
 }
