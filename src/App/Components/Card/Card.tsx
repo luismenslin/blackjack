c007e240395979cd26487595bc740a923a9c3cc1
Vinicius <viniciuswillealves@gmail.com>
2024-05-06 21:38:02 -0300

    feat: adaptando overflow de cartas para mobile

--- src/App/Components/Card/Card.tsx
+++ src/App/Components/Card/Card.tsx
@@ -1,19 +1,25 @@
 import styles from  "./Card.module.scss"
 import Suit from "../Suit";
-import React from "react";
+import React, {useContext} from "react";
+import cx from "classnames";
+import {AppContext} from "../../context/AppContext";
 
 export interface cardProps {
     suit: string
     number: string
     color: string
     isDown?: boolean
+    handSize?: number
 }
 
-const Card = ({suit, number, color, isDown=false} : cardProps) => {
+const Card = ({suit, number, color, isDown=false, handSize} : cardProps) => {
+    const { isMobile } = useContext(AppContext)
+    const cardMarginStyle = (handSize && handSize >= 3 && isMobile) ? styles.cardMargin : ''
+
     return (
        <>
            {isDown &&
-               <div className={styles.card}>
+               <div className={cx(styles.card, cardMarginStyle)}>
                     <div className={styles.cardDown}>
                         <div className={styles.cardDown}>
                         </div>
@@ -22,7 +28,7 @@ const Card = ({suit, number, color, isDown=false} : cardProps) => {
            }
 
            {!isDown &&
-               <div className={styles.card}>
+               <div className={cx(styles.card, cardMarginStyle)}>
                    <div className={styles.numberTop}>
                        <div style={{color: color}}>{number}</div>
                    </div>
